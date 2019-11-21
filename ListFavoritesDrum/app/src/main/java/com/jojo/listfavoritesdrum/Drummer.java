f6af497f4cb4ae45c0ea4592e851a0d8af8aed57
Josua Pangihutan Sitanggang <josuastg93@gmail.com>
2019-11-21 11:46:04 +0700

    List Drum

---
+++ ListFavoritesDrum/app/src/main/java/com/jojo/listfavoritesdrum/Drummer.java
@@ -0,0 +1,43 @@
+package com.jojo.listfavoritesdrum;
+
+public class Drummer {
+
+    private String name;
+    private String detail;
+    private int photo;
+    private  String video;
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+
+}
